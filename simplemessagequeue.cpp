e7bb687b3e3886eae6cbeda9a8b9f94c0bb4faf9
Joan Rivas <dairo.rivas@udea.edu.co>
2022-04-08 15:39:41 -0500

    example 1

--- simplemessagequeue.cpp
+++ simplemessagequeue.cpp
@@ -2,66 +2,47 @@
 #include <condition_variable>
 #include <queue>
 #include <memory>
-namespace messaging
-{
-struct message_base
-{
-virtual ~message_base()
-{}
-};
 
-template<typename Msg>
-struct wrapped_message:
-message_base
-{
-Msg contents;
-explicit wrapped_message(Msg const& contents_):
-contents(contents_)
-{}
-};
-class queue
-{
-std::mutex m;
-std::condition_variable c;
-std::queue<std::shared_ptr<message_base> > q;
-public:
-template<typename T>
-void push(T const& msg)
-{
-std::lock_guard<std::mutex> lk(m);
-q.push(std::make_shared<wrapped_message<T> >(msg));
-c.notify_all();
-}
-std::shared_ptr<message_base> wait_and_pop()
-{
-std::unique_lock<std::mutex> lk(m);
-c.wait(lk,[&]{return !q.empty();});
-auto res=q.front();
-q.pop();
-return res;
-}
-};
-}
 
 namespace messaging
 {
-class sender
-{
-queue*q;
-public:
-sender():
-q(nullptr)
-{}
-explicit sender(queue*q_):
-q(q_)
-{}
-template<typename Message>
-void send(Message const& msg)
-{
-if(q)
-{
-q->push(msg);
-}
-}
-};
+    struct message_base
+    {
+        virtual ~message_base(){
+        }   
+    };
+
+    template<typename Msg>
+    struct wrapped_message:message_base
+    {
+        Msg contents;
+        explicit wrapped_message(Msg const& contents_):
+        contents(contents_){
+        }
+    };
+
+    class queue
+    {
+        std::mutex m;
+        std::condition_variable c;
+        std::queue<std::shared_ptr<message_base> > q;
+
+        public:
+            template<typename T>
+            void push(T const& msg)
+        {
+        std::lock_guard<std::mutex> lk(m);
+        q.push(std::make_shared<wrapped_message<T> >(msg));
+        c.notify_all();
+        }
+
+        std::shared_ptr<message_base> wait_and_pop()
+        {
+            std::unique_lock<std::mutex> lk(m);
+            c.wait(lk,[&]{return !q.empty();});
+            auto res=q.front();
+            q.pop();
+            return res;
+        }
+    };
 }
